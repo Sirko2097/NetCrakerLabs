127a08aea932cae2fb72a7538579011a2dd4ad7b
Sirko2097 <2097_Cthusq>
2018-12-01 21:17:45 +0200

    Timeout test

--- src/test/java/firstpractise/analyzer/AnalyzerTest.java
+++ src/test/java/firstpractise/analyzer/AnalyzerTest.java
@@ -9,39 +9,38 @@ import firstpractise.sorters.ReverseBubbleSorter;
 import org.junit.Test;
 
 public class AnalyzerTest {
-    private int[] arr = new int[9999];
 
     @Test(timeout = 10000)
     public void analyze() {
         new Filler(9999);
-        arr = Filler.getArrayWithRandomLocatedElements();
+        int[] arr = Filler.getArrayWithRandomLocatedElements();
 
         BubbleSorter bubbleSorter = new BubbleSorter(arr);
         bubbleSorter.sort();
 
-        ReverseBubbleSorter reverse = new ReverseBubbleSorter(this.arr);
+        ReverseBubbleSorter reverse = new ReverseBubbleSorter(arr);
         reverse.sort();
 
-        QuickSorter quickSorter = new QuickSorter(this.arr);
+        QuickSorter quickSorter = new QuickSorter(arr);
         quickSorter.sort();
 
 
-        MergeSorter mergeSorter = new MergeSorter(this.arr);
+        MergeSorter mergeSorter = new MergeSorter(arr);
         mergeSorter.bothBubbleSort();
 
-        mergeSorter.setArr(this.arr);
+        mergeSorter.setArr(arr);
         mergeSorter.bubbleAndQuickSort();
 
-        mergeSorter.setArr(this.arr);
+        mergeSorter.setArr(arr);
         mergeSorter.bubbleAndArraysSort();
 
-        mergeSorter.setArr(this.arr);
+        mergeSorter.setArr(arr);
         mergeSorter.reverseBubbleAndQuickSort();
 
-        mergeSorter.setArr(this.arr);
+        mergeSorter.setArr(arr);
         mergeSorter.reverseBubbleAndArraysSort();
 
-        mergeSorter.setArr(this.arr);
+        mergeSorter.setArr(arr);
         mergeSorter.quickAndArraysSort();
 
     }
